95198a5120dfe3e432d3a65cb0eb9931459f6389
John Dust <d24dust@hotmail.com>
2021-02-23 20:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  const input = document.querySelector('#input')
+  input.addingEventListener('click', callback(event))
+}
+
+function callback(event) {
+  alert("I was clicked!")
 }
